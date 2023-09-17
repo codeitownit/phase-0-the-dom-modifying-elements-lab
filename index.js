076bdd875c42a90825b60e62e2bf26af230080f4
Everlyn <gachuieve32@gmail.com>
2023-09-17 20:27:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Write your code here!
 
+//removes main node
 document.querySelector("main#main").remove();
 
+//creates h1 node
 const newHeader=document.createElement("h1");
 
+// adds id to h1
 newHeader.id="victory";
 
-newHeader.innerHTML="Everlyn is the champion";
+//adds text to h1 
+newHeader.innerHTML="<p>Everlyn is the champion</p>";
